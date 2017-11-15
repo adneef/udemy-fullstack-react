667083bfa2d38cc1b84ac9126f3ffc8f1d69a179
Adam Neef <adneef@gmail.com>
2017-11-15 14:09:05 -0700

    working on setting up db

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-// const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 const cookieSession = require('cookie-session')
 const passport = require('passport')
 const keys = require('./config/keys')
@@ -7,18 +7,18 @@ var session = require('express-session')
 require('./models/User')
 require('./services/passport')
 
-// mongoose.connect(keys.mongoURI)
+mongoose.connect(keys.mongoURI)
 
 const app = express()
 
-app.use(
-  cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000
-  })
-)
+// app.use(
+//   cookieSession({
+//     maxAge: 30 * 24 * 60 * 60 * 1000
+//   })
+// )
 
 require('./routes/authRoutes')(app)
 require('./routes/twitterRoutes')(app)
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 app.listen(PORT)
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,41 +1,56 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 const TwitterStrategy = require('passport-twitter').Strategy
-// const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 const keys = require('../config/keys')
 
-// const User = mongoose.model('users')
+const User = mongoose.model('users')
+
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => {
+      done(null, user)
+    })
+})
 
 passport.use(
   new GoogleStrategy(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
+      callbackURL: 'http://localhost:3000/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      // User.findOne({ googleId: profile.id }).then(existingUser => {
-      //   if (existingUser) {
-      //     done(null, existingUser)
-      //   } else {
-      //     new User({ googleId: profile.id })
-      //       .save()
-      //       .then(user => done(null, user))
-      //   }
-      // })
+      console.log("access token:", accessToken)
+      console.log("refresh token:", refreshToken)
+      console.log("profile:", profile)
+      User.findOne({ googleId: profile.id })
+        .then((existingUser) => {
+          if(existingUser) {
+            done(null, existingUser)
+          } else {
+            new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user))
+          }
+        })
     }
   )
 )
 
-passport.use(new TwitterStrategy({
-    consumerKey: keys.twitterConsumerKey,
-    consumerSecret: keys.twitterConsumerSecret,
-    callbackURL: "/auth/twitter/callback"
-  },
-  function(token, tokenSecret, profile, cb) {
-    console.log('token from twitter:', token);
-    // User.findOrCreate({ twitterId: profile.id }, function (err, user) {
-    //   return cb(err, user);
-    // });
-  }
-));
+// passport.use(new TwitterStrategy({
+//     consumerKey: keys.twitterConsumerKey,
+//     consumerSecret: keys.twitterConsumerSecret,
+//     callbackURL: "/auth/twitter/callback"
+//   },
+//   function(token, tokenSecret, profile, cb) {
+//     console.log('token from twitter:', token);
+//     // User.findOrCreate({ twitterId: profile.id }, function (err, user) {
+//     //   return cb(err, user);
+//     // });
+//   }
+// ));
